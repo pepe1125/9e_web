f2e3b0ec37e9cfa43ae0b047638f81f5c04020c9
TrencsanszkyPatrik <90695000+TrencsanszkyPatrik@users.noreply.github.com>
2022-03-05 15:38:13 +0100

    s

---
+++ js/open.js
@@ -0,0 +1,24 @@
+function ferfi() {
+
+    window.open('ferfi.html')
+
+    let new_window =
+        open(location, '_self');
+
+    new_window.close();
+
+    return false;
+
+}
+
+function noi(){
+    window.open('noi.html')
+
+    let new_window =
+        open(location, '_self');
+
+    new_window.close();
+
+    return false;
+
+}
